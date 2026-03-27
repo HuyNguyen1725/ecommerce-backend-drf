8b7e830dd49ca6757222da2910832cf77eef6bfd
HuyNguyen1725 <0702huy@gmail.com>
2026-03-27 22:09:33 +0700

    fix blog serializer

--- blogs/serializers.py
+++ blogs/serializers.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 from .models import Blog, BlogComment, Rate
 
 class BlogSerializer(serializers.ModelSerializer):
-    # time = serializers.TimeField(format="%-I:%M%p", read_only=True)
-    # date = serializers.DateField(format="%B %-d, %Y", read_only=True)
+    time = serializers.TimeField(format="%-I:%M%p", read_only=True)
+    date = serializers.DateField(format="%B %-d, %Y", read_only=True)
     class Meta:
         model = Blog
         fields = "__all__"
